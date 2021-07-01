f07f109e38a6514c49563950f859ac17b62309c4
Randy <randykeithlittle@gmail.com>
2021-07-01 02:25:45 -0500

    ADD basic project structure/boilerplate

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.scss";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import GlobalContext from "./context/Global";
+import Router from "./context/Router";
+import routes from "./routes";
+import "./styles/app.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalContext>
+      <Router routes={routes} />
+    </GlobalContext>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
