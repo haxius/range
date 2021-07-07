0eeb05b11d31b83a2ca317595c44151ce8ead0a8
Randy <randykeithlittle@gmail.com>
2021-07-07 18:10:56 -0500

    UPDATE Battery Component, ADD styles

--- src/components/Battery/index.tsx
+++ src/components/Battery/index.tsx
@@ -8,25 +8,28 @@ const Battery: FC<IBatteryProps> = () => {
   const [values, setValues] = useState<number[]>([50]);
 
   return (
-    <Range
-      step={0.1}
-      min={0}
-      max={100}
-      values={values}
-      onChange={(values) => setValues(values)}
-      renderTrack={({ props, children }) => (
-        <div {...props} className={styles.track} style={props.style}>
-          {children}
-          <div
-            className={styles.completion}
-            style={{ width: `${values[0]}%` }}
-          />
-        </div>
-      )}
-      renderThumb={({ props }) => (
-        <div {...props} className={styles.thumb} style={props.style} />
-      )}
-    />
+    <div className={styles.battery}>
+      <Range
+        step={0.1}
+        min={0}
+        max={100}
+        values={values}
+        onChange={(values) => setValues(values)}
+        renderTrack={({ props, children }) => (
+          <div {...props} className={styles.track} style={props.style}>
+            {children}
+            <div
+              className={styles.completion}
+              style={{ width: `${values[0]}%` }}
+            />
+          </div>
+        )}
+        renderThumb={({ props }) => (
+          <div {...props} className={styles.thumb} style={props.style} />
+        )}
+      />
+      <div className={styles.batteryCap} />
+    </div>
   );
 };
 
