d11311b5396a9e1afbf2b03f521cda17158cc20a
Randy <randykeithlittle@gmail.com>
2021-07-08 01:21:24 -0500

    ADD ClassCat, various styling changes for Battery component

--- src/components/Battery/index.tsx
+++ src/components/Battery/index.tsx
@@ -1,3 +1,4 @@
+import cc from "classcat";
 import React, { FC, useState } from "react";
 import { Range } from "react-range";
 import styles from "./styles.module.scss";
@@ -9,7 +10,8 @@ const Battery: FC<IBatteryProps> = () => {
 
   const updateValues = (newValues: number[]): void => {
     const [newValue] = newValues || [50];
-    setValue(newValue < 50 ? 50 : newValue);
+    let result = newValue % 10 < 3 ? newValue - (newValue % 10) : newValue;
+    setValue(result < 50 ? 50 : result);
   };
 
   return (
@@ -24,6 +26,26 @@ const Battery: FC<IBatteryProps> = () => {
           <div {...props} className={styles.track} style={props.style}>
             {children}
             <div className={styles.completion} style={{ width: `${value}%` }} />
+            <div
+              className={cc([styles.guide, styles.guideGreen])}
+              style={{ left: "50%" }}
+            >
+              <span>50%</span>
+            </div>
+            <div className={styles.guide} style={{ left: "60%" }} />
+            <div
+              className={cc([styles.guide, styles.guideGreen])}
+              style={{ left: "70%" }}
+            >
+              <span>70%</span>
+            </div>
+            <div className={styles.guide} style={{ left: "80%" }} />
+            <div
+              className={cc([styles.guide, styles.guideGreen])}
+              style={{ left: "90%" }}
+            >
+              <span>DAILY</span>
+            </div>
           </div>
         )}
         renderThumb={({ props }) => (
